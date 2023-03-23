f32fa4eb03e98ff81550cb781d0f119ae129d68b
Diptonil Roy <diptonilr@gmail.com>
2023-03-23 12:17:08 +0530

    Start first Flutter app

---
+++ first_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RootWidget());
+}
+
+class RootWidget extends StatelessWidget {
+  const RootWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
